0ac01ee1249c98da6ce15784e183e6181e74f57e
Jack Randol <jackrandol@gmail.com>
2020-05-14 17:32:51 +0200

    Updates

---
+++ precache-manifest.ea196b2355e83fe99c08ec7e98fa798e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3fa8e68c5993d5ab06cd2f5b1571bc",
+    "url": "/jackrandol/jackr-portfolio/index.html"
+  },
+  {
+    "revision": "b653e8e88479cec74dd3",
+    "url": "/jackrandol/jackr-portfolio/static/css/main.727b17bc.chunk.css"
+  },
+  {
+    "revision": "5558da925b7dd7c26654",
+    "url": "/jackrandol/jackr-portfolio/static/js/2.d7d40218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jackrandol/jackr-portfolio/static/js/2.d7d40218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b653e8e88479cec74dd3",
+    "url": "/jackrandol/jackr-portfolio/static/js/main.1f094b35.chunk.js"
+  },
+  {
+    "revision": "543a755731dbccdcaf0f",
+    "url": "/jackrandol/jackr-portfolio/static/js/runtime-main.b0ecd574.js"
+  },
+  {
+    "revision": "b63f9505dd8b916db8cf5db6efe843f4",
+    "url": "/jackrandol/jackr-portfolio/static/media/JRWrapper2.b63f9505.jpg"
+  }
+]);
